450d4f61ff5f1ccbcd85250d09d7ffa6bef0c39b
musawakiliML <musawakiliml@hotmail.com>
2022-09-08 23:19:51 +0100

    Configured routes

--- Todos API/api.py
+++ Todos API/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from todo import todo_router
 
 app = FastAPI()
 
@@ -6,3 +7,5 @@ app = FastAPI()
 @app.get('/')
 async def index() -> dict:
     return {"message": "Welcome to my Todo API"}
+
+app.include_router(todo_router)
