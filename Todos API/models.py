66d76fb87243593b4cbb5f7d7b2f284e15c5fb01
musawakiliML <musawakiliml@hotmail.com>
2022-09-09 01:28:43 +0100

    added exmaple set to allow user understand the schema

--- Todos API/models.py
+++ Todos API/models.py
@@ -1,10 +1,32 @@
 from pydantic import BaseModel
 
 # Define our Todo API Data Model
+
+
 class Item(BaseModel):
     item: str
     status: str
 
+    class Config:
+        Schema_extra = {
+            'Example': {
+                'item': "your todo",
+                'status': 'completed'
+            }
+        }
+
+
 class Todo(BaseModel):
     id: int
     item: Item
+
+    class Config:
+        Schema_extra = {
+            'Example': {
+                'id': 1,
+                'item': {
+                    'item': "your todo",
+                    'status': 'completed'
+                }
+            }
+        }
