7d12d4a3ddc3156d71d5614b4e73a73114e5f2ec
musawakiliML <musawakiliml@hotmail.com>
2022-10-31 00:50:02 +0100

    Added test endpoint

--- Test API/api.py
+++ Test API/api.py
@@ -2,7 +2,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get('/')
-async def index() -> dict:
-    return {"data": "Hello World!!"}
+async def get_items():
+    return ['item 1', 'item 2', 'item 3']
+
+
+
+
+
+
+
+
+
+
+#@app.get('/')
+#async def index() -> dict:
+#    return {"data": "Hello World!!"}
