fbf33447237d4d02cf5f22160e3e6e7e08a16098
musawakiliML <musawakiliml@hotmail.com>
2022-10-31 01:19:42 +0100

    Added post route

--- Test API/api.py
+++ Test API/api.py
@@ -1,35 +1,42 @@
 from fastapi import FastAPI
+from models import Item
 
 app = FastAPI()
 
 test_database = {
-    1:{"task":"read books"},
-    2:{"task":"Write blog"},
-    3:{"task":"Create a youtube stream"},
+    1: {"task": "read books"},
+    2: {"task": "Write blog"},
+    3: {"task": "Create a youtube stream"},
 }
 
 # get all items
+
+
 @app.get('/')
 async def get_items():
     return test_database
 
 # get a single item
+
+
 @app.get("/{id}")
-async def get_item(id:int):
+async def get_item(id: int):
     if id in test_database.keys():
         return test_database[id]
     else:
         return "Invalid ID"
 
 # post an item option 1
+
+
 @app.post('/')
-async def add_item(task:str):
+async def add_item(item: Item):
+    new_id = len(test_database.keys()) + 1
+    test_database[new_id] = item
+
+    return test_database
 
 
-
-
-
-
-#@app.get('/')
-#async def index() -> dict:
+# @app.get('/')
+# async def index() -> dict:
 #    return {"data": "Hello World!!"}
